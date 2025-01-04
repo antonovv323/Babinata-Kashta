5bfc2e56a5d396f0f551c16516d0e5dae9aff096
antonovv323 <toni.antonov23@gmail.com>
2025-01-04 15:11:22 +0200

    Сайт за "Бабината Къща" в с. Бели Осъм

--- script.js
+++ script.js
@@ -8,31 +8,6 @@ window.addEventListener('load', () => {
 
 // ----------------------------------------- //
 
-// REMOVING THE - ELEMENT //
-
-function debounce(func, delay) {
-    let timeout;
-    return function() {
-        clearTimeout(timeout);
-        timeout = setTimeout(func, delay);
-    };
-}
-
-window.addEventListener('resize', debounce(function() {
-    if (window.innerWidth >= 320 && window.innerWidth <= 1366) {
-        const phoneTexts = document.querySelectorAll('.phone-text');
-        phoneTexts.forEach(function(element) {
-            element.textContent = element.textContent.replace(/-/g, '').replace(/\s+/g, '');
-        });
-    } else {
-        const phoneTexts = document.querySelectorAll('.phone-text');
-        phoneTexts.forEach(function(element) {
-        });
-    }
-}, 200));
-
-// ----------------------------------------- //
-
 //SHOWING THE EFFECTS//
 
 window.addEventListener('load', () => {
